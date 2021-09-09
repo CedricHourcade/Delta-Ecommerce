fdd6f79c0958f72bb8c23bbbe14fa38cb7c521ad
CedricHourcade <cedric.hourcade1@gmail.com>
2021-09-09 18:51:18 +0200

    Ajout des constraints formulaire register

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -11,6 +11,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class RegisterType extends AbstractType
 {
@@ -19,25 +20,48 @@ class RegisterType extends AbstractType
         $builder
             ->add('firstname', TextType::class, [
                 'label' => 'Votre prénom',
+                'constraints' => new Length([
+                    'min' => 1,
+                    'max' => 30,
+                    'minMessage' => 'Votre prénom doit contenir minimum 1 caractères',
+                    'maxMessage' => 'Votre prénom doit contenir maximum 30 caractères'
+                ]),
                 'attr' => [
                     'placeholder' => 'Entrer votre prénom'
                 ]
             ])
             ->add('lastname', TextType::class, [
                 'label' => 'Votre nom',
+                'constraints' => new Length([
+                    'min' => 1,
+                    'max' => 30,
+                    'minMessage' => 'Votre nom doit contenir minimum 1 caractères',
+                    'maxMessage' => 'Votre nom doit contenir maximum 30 caractères'
+                ]),
                 'attr' => [
                     'placeholder' => 'Entrer votre nom'
                 ]
             ])
             ->add('email', EmailType::class, [
                 'label' => 'Email',
+                'constraints' => new Length([
+                    'min' => 1,
+                    'max' => 30,
+                    'minMessage' => 'Votre email doit contenir minimum 1 caractères',
+                    'maxMessage' => 'Votre email doit contenir maximum 30 caractères'
+                ]),
                 'attr' => [
                     'placeholder' => 'Entrer votre adresse email'
                 ]
             ])
             ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
-                'invalid_message' => 'Le mot de passe et la confirmation doivent être identique',
+                'constraints' => new Length([
+                    'min' => 2,
+                    'max' => 30,
+                    'minMessage' => 'Votre mot de passe doit contenir minimum 1 caractères',
+                    'maxMessage' => 'Votre mot de passe doit contenir maximum 30 caractères'
+                ]),
                 'label' => 'Mot de passe',
                 'required' => true,
                 'first_options' => [
